b305e112f45bddbf5b7a820a928d55a79cbb10b6
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-12-05 16:13:40 +0000

    create review WIP

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :products
   resources :reviews
 
+  get "/new" => "reviews#new", as: :new
   get "/:username" => "users#show", as: :user
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
