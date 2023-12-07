a67d02373ffc351f8f251a12dc922623bcd74c28
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-12-07 21:10:49 +0000

    defined which reviews users can see

--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -5,4 +5,14 @@ class ReviewPolicy
     @user = user
     @review = review
   end
+
+  # Our policy is that a photo should only be seen by the owner or followers
+  #   of the owner, unless the owner is not private in which case anyone can
+  #   see it
+  def show?
+    user == review.owner ||
+      !(review.visibility == 'only_me') ||
+      review.owner.followers.include?(user) ||
+      review.visibility == 'everyone'
+  end
 end
