ed9c108c920ca5d12c3ad3dbaa3737c09b395ec8
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-12-08 22:25:46 +0000

    restricted follow request routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :media
-  resources :follow_requests
+  resources :follow_requests, except: [:index, :show, :new, :edit]
   resources :products
   resources :reviews
 
