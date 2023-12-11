6a20daa74da3953aedee8a16eed5547eee9702c4
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-12-11 21:28:27 +0000

    added username field to sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@ class ApplicationController < ActionController::Base
   include Pundit
   skip_forgery_protection
   before_action :authenticate_user!
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 
   private
 
